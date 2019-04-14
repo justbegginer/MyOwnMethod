67086472591873d11dbffa8860f700427ffbb6fc
justbegginer <46086383+justbegginer@users.noreply.github.com>
2019-04-14 16:10:37 +0300

    forMe

---
+++ Create_palidrome/Create_palidrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MyMethod;
+namespace MyOwnMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().SubstringReturn(0,2);
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
+namespace MyMethod
+{
+    public static class StringOperation
+    {
+        public static string SubstringReturn(this String str , int index , int length )
+        {
+            string result = "";
+            for (int counter = 0 ; counter<length ; counter++)
+            {
+                result += str[index + counter];
+            }
+            return result;
+        }
+    }
+
+}
